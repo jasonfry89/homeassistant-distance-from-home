4c01bf975565945cd1fa9932ae730b0faae4f3f7
Jason Fry <jasonfry89@gmail.com>
2026-08-09 11:49:27 -0500

    Fix keys

--- custom_components/distance_from_home/sensor.py
+++ custom_components/distance_from_home/sensor.py
@@ -46,7 +46,7 @@ class DistanceSensor(CoordinatorEntity[DistanceUpdateCoordinator], SensorEntity)
     ) -> None:
         """Initialize the sensor."""
         super().__init__(coordinator)
-        self._key = f"{mode}"
+        self._key = f"{coordinator.title}_{mode}"
         self._attr_unique_id = self._key
         self._attr_translation_key = f"{mode}_distance"
         self._attr_device_info = DeviceInfo(
